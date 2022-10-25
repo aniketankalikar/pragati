c16ca2df8d5ce62f36c6e97bfb4e89f6f67149a2
aniketankalikar <aniket.ankalikar@gmail.com>
2022-10-25 19:06:34 +0530

    pragati files

---
+++ src/main/java/leetcode/beginner/SumArray1D.java
@@ -0,0 +1,25 @@
+package leetcode.beginner;
+
+public class SumArray1D {
+	
+	public static void main(String args[])
+	{
+		runningSum(new int [] {1,2,3,4});
+	}
+	
+    public static int[] runningSum(int[] nums) {
+        
+        int [] result = new int[nums.length];
+        int sum =0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum = 0;
+            for(int j = 0; j<=i; j++)
+            {
+                sum += nums[j];
+            }
+            result [i] = sum;
+        }
+        return result;
+    }
+}
