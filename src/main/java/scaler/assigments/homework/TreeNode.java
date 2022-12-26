843b44e20fa9b6fe83f727c7916e18ca6a8b1217
aniketankalikar <aniket.ankalikar@gmail.com>
2022-12-26 08:44:09 +0530

    more scalar problems

---
+++ src/main/java/scaler/assigments/homework/TreeNode.java
@@ -0,0 +1,15 @@
+package scaler.assigments.homework;
+
+public class TreeNode {
+
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+		left = null;
+		right = null;
+	}
+
+}
---
+++ src/main/java/scaler/assigments/homework/Tree_Assignment_Q2.java
@@ -0,0 +1,57 @@
+package scaler.assigments.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Tree_Assignment_Q2 {
+
+	
+	// PRE ORDER
+	
+	public static void main(String args[])
+	{
+		TreeNode head = new TreeNode(1);
+		TreeNode left = new TreeNode(2);
+		TreeNode right = new TreeNode(3);
+		head.left= left;
+		head.right = right;
+		TreeNode node = new TreeNode(4);
+		TreeNode node1 = new TreeNode(5);
+		right.left = node;
+		node.right = node1;
+		
+		int [] res = new Tree_Assignment_Q1().inorderTraversal(head);
+		System.out.println(res);
+	}
+	
+	public int[] inorderTraversal(TreeNode A) {
+		List<Integer> list = new ArrayList<Integer>();
+
+		Stack<TreeNode> s = new Stack<TreeNode>();
+		s.push(A);
+		
+		while(s.size()>0)
+		{
+			TreeNode current = s.pop();
+			list.add(current.val);
+			
+			if (current.right != null) {
+                s.push(current.right);
+            }
+            if (current.left != null) {
+                s.push(current.left);
+            }
+			
+		}
+		
+		int [] result = new int[list.size()];
+		for(int i=0;i<list.size();i++)
+		{
+			result[i] = list.get(i);
+		}
+		
+		return result;
+	}
+
+}
