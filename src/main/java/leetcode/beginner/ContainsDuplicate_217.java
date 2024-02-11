ed1569ca08b56ba29bb2e24f2d2e4434c8e3cb35
Aniket <aniket@clayfin.com>
2024-02-12 01:40:23 +0530

    Leetcode problems

---
+++ src/main/java/leetcode/beginner/ContainsDuplicate_217.java
@@ -0,0 +1,26 @@
+package leetcode.beginner;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate_217 {
+
+    public static void main(String args[])
+    {
+        int nums [] = {1,1,1,3,3,4,3,2,4,2};
+        boolean result = new ContainsDuplicate_217().containsDuplicate(nums);
+        System.out.println(result);
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+                   return true;
+        }
+
+        return false;
+    }
+}
